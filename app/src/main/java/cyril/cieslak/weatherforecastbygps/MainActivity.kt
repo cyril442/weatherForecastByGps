9c5fab77f0d04fba838a52ef184cc239fe48c147
Cyril442 <cyril.cieslak@gmail.com>
2019-07-31 11:27:00 +0200

    Fragments added to the Main Activity Tabs

--- app/src/main/java/cyril/cieslak/weatherforecastbygps/MainActivity.kt
+++ app/src/main/java/cyril/cieslak/weatherforecastbygps/MainActivity.kt
@@ -1,12 +1,160 @@
 package cyril.cieslak.weatherforecastbygps
 
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.support.design.widget.TabLayout
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.GravityCompat
+import android.support.v4.view.ViewPager
+import android.support.v4.widget.DrawerLayout
+import android.view.View
+import com.google.android.gms.location.*
+import cyril.cieslak.weatherforecastbygps.Fragments.NowFragment
+import cyril.cieslak.weatherforecastbygps.Fragments.Today_Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+
+  //  private var sharedViewModel: SharedViewModel? = null
+
+    lateinit var fusedLocationProviderClient : FusedLocationProviderClient
+    lateinit var locationRequest : LocationRequest
+    lateinit var locationCallback : LocationCallback
+
+    val REQUEST_CODE = 1000
+
+
+    lateinit var drawer: DrawerLayout
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Manage the NavigationView with TabLayout and viewPager
+        var tabLayout = findViewById<TabLayout>(R.id.tabs)
+        var pager = findViewById<ViewPager>(R.id.viewPager)
+
+
+        // Page Adapter
+        val adapter = MyViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(NowFragment(), "Now")
+        adapter.addFragment(Today_Fragment(), "Today")
+//        adapter.addFragment(FragmentSports(), "Sports")
+//        adapter.addFragment(FragmentWeather(), "Meteo")
+        viewPager.adapter = adapter
+        tabs.setupWithViewPager(viewPager)
+
+        // Check Permission for GPS point
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION))
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE)
+
+        else
+        {
+            buildLocationRequest()
+            buildLocationCallBack()
+
+//            val currentLat = "cuutt"
+//            val currentLongi = "jfklhfdlkf"
+
+
+
+
+            // Create FusedProviderClient
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+            // Set Event Click on Update Button
+            button_update_coordinates.setOnClickListener (View.OnClickListener {
+
+                if(ActivityCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED   )
+                {
+                    ActivityCompat.requestPermissions(this@MainActivity, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE)
+                    return@OnClickListener
+                }
+                fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+
+
+
+
+            })
+        }
+
     }
+
+    override fun onBackPressed() {
+
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    // CLASS To MANAGE the PageAdapter
+    class MyViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+
+        private val fragmentList: MutableList<Fragment> = ArrayList()
+        private val titleList: MutableList<String> = ArrayList()
+
+        override fun getItem(position: Int): Fragment {
+            return fragmentList[position]
+        }
+
+        override fun getCount(): Int {
+            return fragmentList.size
+        }
+
+        fun addFragment(fragment: Fragment, title: String) {
+            fragmentList.add(fragment)
+            titleList.add(title)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return titleList[position]
+        }
+    }
+
+    private fun buildLocationCallBack() {
+        locationCallback = object : LocationCallback() {
+
+            override fun onLocationResult(p0: LocationResult?) {
+                var location = p0!!.locations.get(p0!!.locations.size-1)
+                yourCurrentLatitude.text = location.latitude.toString()
+                yourCurrentLongitude.text = location.longitude.toString()
+
+                val currentLat = location.latitude.toString()
+                val currentLongi = location.longitude.toString()
+
+//                val bundle = Bundle()
+//                val lat = "$currentLat"
+//                val longi = "$currentLongi"
+//                bundle.putString("latitude", lat)
+//                bundle.putString("longitude", longi)
+
+            //    sharedViewModel?.latitude?.postValue(currentLat)
+
+
+            }
+
+        }
+
+    }
+
+    private fun buildLocationRequest() {
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 5000
+        locationRequest.fastestInterval = 3000
+        locationRequest.smallestDisplacement = 10f
+
+    }
+
 }
