491beb1d69b1ac28b055cac578f064be972705ab
Cyril442 <cyril.cieslak@gmail.com>
2019-08-30 17:16:52 +0200

    Cleaned

--- app/src/main/java/androidviakotlin/blog/weatherforecastbygps/Utils/Parsers/parseDatasWeatherNextFiveDays.kt
+++ app/src/main/java/androidviakotlin/blog/weatherforecastbygps/Utils/Parsers/parseDatasWeatherNextFiveDays.kt
@@ -1,10 +1,7 @@
 package androidviakotlin.blog.weatherforecastbygps.Utils.Parsers
 
 
-import android.app.Activity
-import android.content.res.Resources
-import android.support.v4.content.res.TypedArrayUtils.getString
-import android.support.v4.content.res.TypedArrayUtils.getText
+
 import android.util.Log
 import androidviakotlin.blog.weatherforecastbygps.R
 import org.json.JSONArray
@@ -90,75 +87,6 @@ class parseDatasWeatherNextFiveDays() {
             Log.i("banga1", "temperature : $temperature")
 
 
-            /// TEST TEMPERATURE
-//
-//            endTemperature = "test"
-//            var endTemp = "testEndTemp"
-//            var temp = ArrayList<String>()
-//
-//            val jaTemp = jo.getJSONArray("list")
-//            Log.i("banga2", "jaTemp : $jaTemp")
-//
-//            for (i in 0 until jaTemp.length()) {
-//                val temperatureMain = jaTemp.getJSONObject(i)
-//                Log.i("banga2", "temperatureMain : $temperatureMain")
-//
-//                val temperatureObj = temperatureMain.getJSONObject("main")
-//                Log.i("banga2", "temperatureObj : $temperatureObj")
-//
-//                val temperature = temperatureObj.getString("temp")
-//                Log.i("banga2", "temperature : $temperature")
-//
-//                temp.add(temperature)
-//
-//            }
-//
-//        Log.i("banga2", "TEMP: $temp")
-//        //  var arr = temp.size
-//        var i = 0
-//        var sizeJaTemp = jaTemp.length()
-//        Log.i("banga2", "Value de sizeJaTemp: $sizeJaTemp")
-//
-//
-//            while (i !== sizeJaTemp) {
-//                i++
-//                Log.i("banga2", "Value I: $i")
-//                endTemperature = temp.get(i - 1)
-//                Log.i("banga2", "Value Premier return: $endTemperature")
-//
-//                           }
-//            var temperature = endTemperature
-
-//            val temperatureObject = jo.getJSONObject("main")
-//           val temperatureString = temperatureObject.getString("temp")
-//            val temperatureKelvin = temperatureString.toDouble()
-//            val temperatureCelciusDouble = (temperatureKelvin-KELVIN_TO_CELCIUS).toDouble()
-//          //  val temperatureCelciusRound = String.format("%.2f", temperatureCelciusDouble).toDouble()
-//            val temperatureCelciusString = temperatureCelciusDouble.toString()
-//
-//            Log.i("bingo", "temperatureCelciusString : $temperatureCelciusString")
-
-//            // Get Wind
-//            val windObject = jo.getJSONObject("wind")
-//            val speed = windObject.getString("speed")
-//            Log.i("banga", "wind : $speed")
-
-
-//            // Get Icon Weather
-//            val jaWeatherNow = jo.getJSONArray("weather")
-//
-//            for (i in 0 until jaWeatherNow.length()) {
-//                val job = jaWeatherNow.getJSONObject(i)
-//
-//                val icon_weather = job.getString("icon")
-//                Log.i("bingo", "icon : $icon_weather")
-
-
-//                val section = jo.getString("section")
-//                val subsection = jo.getString("subsection")
-//                val updated_date = jo.getString("updated_date")
-//                val urlArticle = jo.getString("url")
-
 
             // Get TimeStamp
             val jaTimestamp = jo.getJSONArray("list")
@@ -169,81 +97,6 @@ class parseDatasWeatherNextFiveDays() {
                 Log.i("banga1", "timestamp : $timestampNotFormated")
 
                 val timestamp = getTheCorrectDateFormat(timestampNotFormated)
-
-
-                // Get Temperature and Humidity
-//
-//                val jaTempHum = jo.getJSONArray("list")
-//                for (i in 0 until jaTempHum.length() ) {
-//                    val joTempHum = jaTempHum.getJSONObject(i)
-//
-//                    val jobTempHum = joTempHum.getJSONObject("main")
-//
-//                   // val temperature = jobTempHum.getString("temp")
-//                    val humidity = jobTempHum.getString("humidity")
-//
-//
-//                    Log.i("banga", "humidity : $humidity")
-//
-//
-//                    Log.i("banga", "temprature : $temperature")
-
-                // Get Wind
-
-//
-//                    val jaWind = jo.getJSONArray("list")
-//                    for (i in 0 until jaWind.length()) {
-//
-//                        val joWind = jaWind.getJSONObject(i)
-//                        val jobWind = joWind.getJSONObject("wind")
-//
-//                        var speed = jobWind.getString("speed")
-//
-//                        Log.i("banga", "Wind Speed : $speed")
-
-
-                // get Icon Weather
-
-//                        val jaIcon = jo.getJSONArray("list")
-//                        for (i in 0 until jaIcon.length()-1) {
-//                            val jaWeather = jaIcon.getJSONObject(i)
-//
-////                                    Log.i("banga", "JaWeather : $jaWeather")
-////                                    Log.i("banga", "JaWeather : $jaWeather")
-//
-//                            var jabIcon = jaWeather.getJSONArray("weather")
-//
-//                            for (i in 0 until jabIcon.length()) {
-//                                val joWeather = jabIcon.getJSONObject(i)
-//                                val icon_weather = joWeather.getString("icon")
-//
-//                                Log.i("banga", "Icon Weather : $icon_weather")
-
-//
-//                // See the function Below to know know i the susbestion if prepared for printing
-//            val subsectionReadyToPrint = valueOfTheSubsectionReadyToPrint(subsection)
-//
-//
-//
-//            // See the function bellow transforming the string
-//            val dateToPrint = whatIsTheDateToPrint(updated_date)
-//
-//            val jam = jo.getJSONArray("multimedia")
-//
-//            var urlToPrint: String
-//                if (jam.length() == 0) {
-//                    urlToPrint = "https://i5.photobucket.com/albums/y152/courtney210/wave-bashful_zps5ab77563.jpg"
-//                } else {
-//
-//                    var jom = jam[FIRST_ELEMENT_OF_THE_INDEX] as JSONObject
-//                    var url = jom.getString("url")
-//
-//                    //***--- GET AN IMAGE EVEN WHEN MULTIMEDIA IS EMPTY  ---**//
-//
-//                    when (url) {
-//                        "" -> urlToPrint = "$DUMB_PICTURE_WHEN_NO_PIC_TO_DOWNLOAD"
-//                        else -> urlToPrint = url
-//                    }
 
 
                 var humidity = "42"
@@ -370,7 +223,22 @@ class parseDatasWeatherNextFiveDays() {
 
         }
         Log.i("banga2", "TEMP: $temp")
-        //  var arr = temp.size
+
+
+        for (temmp in temp) {
+
+            endTemperature = temmp
+
+
+            Log.i("banga2", "Value without return: $endTemperature")
+
+
+        }
+    }
+
+}
+
+//  var arr = temp.size
 //        var i = 0
 //        var sizeJaTemp = jaTemp.length()
 //        Log.i("banga2", "Value de sizeJaTemp: $sizeJaTemp")
@@ -386,10 +254,6 @@ class parseDatasWeatherNextFiveDays() {
 //        }
 
 
-        for (temmp in temp) {
-
-            endTemperature = temmp
-
 
 //        while (i !== sizeJaTemp) {
 //            i++
@@ -404,7 +268,7 @@ class parseDatasWeatherNextFiveDays() {
 //        return endTemperature
 //        Log.i("banga2", "Value endTempAfter: $endTemp")
 
-            //   }
+//   }
 //
 //    private fun getTheFeckingTemperature() : String{
 //
@@ -412,13 +276,148 @@ class parseDatasWeatherNextFiveDays() {
 //        Log.i("banga2", "Value what is the Fecking Temperature: $temperature")
 //        return temperature
 //    }
-            Log.i("banga2", "Value Second return: $endTemperature")
+
+/// TEST TEMPERATURE
+//
+//            endTemperature = "test"
+//            var endTemp = "testEndTemp"
+//            var temp = ArrayList<String>()
+//
+//            val jaTemp = jo.getJSONArray("list")
+//            Log.i("banga2", "jaTemp : $jaTemp")
+//
+//            for (i in 0 until jaTemp.length()) {
+//                val temperatureMain = jaTemp.getJSONObject(i)
+//                Log.i("banga2", "temperatureMain : $temperatureMain")
+//
+//                val temperatureObj = temperatureMain.getJSONObject("main")
+//                Log.i("banga2", "temperatureObj : $temperatureObj")
+//
+//                val temperature = temperatureObj.getString("temp")
+//                Log.i("banga2", "temperature : $temperature")
+//
+//                temp.add(temperature)
+//
+//            }
+//
+//        Log.i("banga2", "TEMP: $temp")
+//        //  var arr = temp.size
+//        var i = 0
+//        var sizeJaTemp = jaTemp.length()
+//        Log.i("banga2", "Value de sizeJaTemp: $sizeJaTemp")
+//
+//
+//            while (i !== sizeJaTemp) {
+//                i++
+//                Log.i("banga2", "Value I: $i")
+//                endTemperature = temp.get(i - 1)
+//                Log.i("banga2", "Value Premier return: $endTemperature")
+//
+//                           }
+//            var temperature = endTemperature
+
+//            val temperatureObject = jo.getJSONObject("main")
+//           val temperatureString = temperatureObject.getString("temp")
+//            val temperatureKelvin = temperatureString.toDouble()
+//            val temperatureCelciusDouble = (temperatureKelvin-KELVIN_TO_CELCIUS).toDouble()
+//          //  val temperatureCelciusRound = String.format("%.2f", temperatureCelciusDouble).toDouble()
+//            val temperatureCelciusString = temperatureCelciusDouble.toString()
+//
+//            Log.i("bingo", "temperatureCelciusString : $temperatureCelciusString")
+
+//            // Get Wind
+//            val windObject = jo.getJSONObject("wind")
+//            val speed = windObject.getString("speed")
+//            Log.i("banga", "wind : $speed")
 
 
-        }
-    }
+//            // Get Icon Weather
+//            val jaWeatherNow = jo.getJSONArray("weather")
+//
+//            for (i in 0 until jaWeatherNow.length()) {
+//                val job = jaWeatherNow.getJSONObject(i)
+//
+//                val icon_weather = job.getString("icon")
+//                Log.i("bingo", "icon : $icon_weather")
 
-}
+
+//                val section = jo.getString("section")
+//                val subsection = jo.getString("subsection")
+//                val updated_date = jo.getString("updated_date")
+//                val urlArticle = jo.getString("url")
 
 
+// Get Temperature and Humidity
+//
+//                val jaTempHum = jo.getJSONArray("list")
+//                for (i in 0 until jaTempHum.length() ) {
+//                    val joTempHum = jaTempHum.getJSONObject(i)
+//
+//                    val jobTempHum = joTempHum.getJSONObject("main")
+//
+//                   // val temperature = jobTempHum.getString("temp")
+//                    val humidity = jobTempHum.getString("humidity")
+//
+//
+//                    Log.i("banga", "humidity : $humidity")
+//
+//
+//                    Log.i("banga", "temprature : $temperature")
+
+// Get Wind
+
+//
+//                    val jaWind = jo.getJSONArray("list")
+//                    for (i in 0 until jaWind.length()) {
+//
+//                        val joWind = jaWind.getJSONObject(i)
+//                        val jobWind = joWind.getJSONObject("wind")
+//
+//                        var speed = jobWind.getString("speed")
+//
+//                        Log.i("banga", "Wind Speed : $speed")
+
+
+// get Icon Weather
+
+//                        val jaIcon = jo.getJSONArray("list")
+//                        for (i in 0 until jaIcon.length()-1) {
+//                            val jaWeather = jaIcon.getJSONObject(i)
+//
+////                                    Log.i("banga", "JaWeather : $jaWeather")
+////                                    Log.i("banga", "JaWeather : $jaWeather")
+//
+//                            var jabIcon = jaWeather.getJSONArray("weather")
+//
+//                            for (i in 0 until jabIcon.length()) {
+//                                val joWeather = jabIcon.getJSONObject(i)
+//                                val icon_weather = joWeather.getString("icon")
+//
+//                                Log.i("banga", "Icon Weather : $icon_weather")
+
+//
+//                // See the function Below to know know i the susbestion if prepared for printing
+//            val subsectionReadyToPrint = valueOfTheSubsectionReadyToPrint(subsection)
+//
+//
+//
+//            // See the function bellow transforming the string
+//            val dateToPrint = whatIsTheDateToPrint(updated_date)
+//
+//            val jam = jo.getJSONArray("multimedia")
+//
+//            var urlToPrint: String
+//                if (jam.length() == 0) {
+//                    urlToPrint = "https://i5.photobucket.com/albums/y152/courtney210/wave-bashful_zps5ab77563.jpg"
+//                } else {
+//
+//                    var jom = jam[FIRST_ELEMENT_OF_THE_INDEX] as JSONObject
+//                    var url = jom.getString("url")
+//
+//                    //***--- GET AN IMAGE EVEN WHEN MULTIMEDIA IS EMPTY  ---**//
+//
+//                    when (url) {
+//                        "" -> urlToPrint = "$DUMB_PICTURE_WHEN_NO_PIC_TO_DOWNLOAD"
+//                        else -> urlToPrint = url
+//                    }
 
