a16b6c6a5dcfb26bbb269abfa8b7f74cab8a24e1
Cyril442 <cyril.cieslak@gmail.com>
2019-09-03 09:16:28 +0200

    Class Model Weather representing the datas

---
+++ app/src/main/java/androidviakotlin/blog/weatherforecastbygps/Utils/Weather.kt
@@ -0,0 +1,22 @@
+package androidviakotlin.blog.weatherforecastbygps.Utils
+
+import java.util.*
+
+class Weather () {
+
+    var cityName: String? = null
+    var country: String? = null
+    var temperature: String? = null
+    var speed: String? = null
+    var icon_weather: String? = null
+    var timestamp: String? = null
+    var humidity: String? = null
+
+
+//    fun setRealDateTime(timestamp : String) : Date {
+//
+//        var newDate : Date
+//
+//        return newDate
+//    }
+}
